0e0836bff1fc8d032245625baca9f2c416f39206
Roberto Santos <robsantos@google.com>
2021-12-02 18:32:39 -0300

    adding youtube scope on setup.py

--- setup.py
+++ setup.py
@@ -22,6 +22,7 @@ from pathlib import Path
 SCOPES = [
     'https://www.googleapis.com/auth/drive.file',
     'https://www.googleapis.com/auth/spreadsheets',
+    'https://www.googleapis.com/auth/youtube',
     'https://www.googleapis.com/auth/script.projects',
 ]
 
