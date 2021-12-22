d55263231097ef23254ef19291fd7df923f33884
Roberto Santos <robsantos@google.com>
2021-12-21 21:10:23 -0300

    adding docker engine

--- cloud_build.sh
+++ cloud_build.sh
@@ -1,3 +1,21 @@
+apt-get remove docker docker-engine docker.io containerd runc
+apt-get update  
+apt-get install \
+    ca-certificates \
+    curl \
+    gnupg \
+    lsb-release
+
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
+  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+
+apt-get update
+apt-get install docker-ce docker-ce-cli containerd.io
+
+
 gcloud services enable \
   drive.googleapis.com \
   script.googleapis.com \
@@ -5,6 +23,11 @@ gcloud services enable \
   youtube.googleapis.com \
   storagetransfer.googleapis.com \
   container.googleapis.com
+
+
+
+
+
 
 # Creation of Drive, Sheets and Appscript
 apt-get install python3-pip nodejs npm gettext -y
