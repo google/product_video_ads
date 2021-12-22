31ce053e0245047ecca624100e98af65b79fde81
Roberto Santos <robsantos@google.com>
2021-12-21 21:35:51 -0300

    adding docker engine

--- cloud_build.sh
+++ cloud_build.sh
@@ -1,7 +1,7 @@
 apt install docker.io -y
 docker --version
 
-curl -LO https://storage.googleapis.com/kubernetes-release/release/$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)/bin/linux/amd64/kubectl && chmod +x ./kubectl && mv ./kubectl /usr/local/bin
+apt install kubectl -y
 kubectl version
 
 gcloud services enable \
