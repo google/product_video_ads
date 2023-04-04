201888bea4228103ddac1fcdc79ce554cbdaa14b
Kuba Rożek <kubarozek@google.com>
2023-04-04 14:47:12 +0200

    format js files

--- appscript/src/MerchantCenter.js
+++ appscript/src/MerchantCenter.js
@@ -15,108 +15,108 @@ limitations under the License.
 */
 
 function retrieveProductsInformation(productIds, contentLanguage, targetCountry) {
-  
-    // Put product ID as key for easy lookup
-    var products = {}
-    var requestEntries = []
-    var batchMerchantIds = {}
-    
-    Log.output('Calling Merchant Center for products: ' + productIds)
-    
-    var product_id_prefix = 'online:' + contentLanguage + ':' + targetCountry + ':'
-    
-    // Create one batch for each product
-    productIds.forEach(function(e, i) {
-    
-      var composed_id = e.split(':')
-      
-      var id = composed_id[0]
-      var merchantId = composed_id[1]
-      var offerId = composed_id[2]
-      
-      requestEntries.push({
-          'batchId': id,
-          'merchantId': merchantId,
-          'method': 'get',
-          'productId': product_id_prefix.concat(offerId)
-        })
-    
-      // Batches of 500
-      if ((i+1) % 500 == 0 || i == productIds.length-1) {
-        
-        Logger.log('Request to shopping API with %s products: %s', requestEntries.length, requestEntries)
-        Log.output('Request to Merchant: ' + JSON.stringify(requestEntries))
-        var shoppingResponse = ShoppingContent.Products.custombatch({'entries': requestEntries})
-      
-        Logger.log('Response from shopping API: ' + JSON.stringify(shoppingResponse).substring(0,1000))
-        var entries = shoppingResponse.entries
-        
-        entries.forEach(function(e) {
-                      
-          try {
-            
-            var product = e.product
-            
-            products[e.batchId] = {
-              title: get_product_title(product), 
-              price: format_brazil_price(get_product_price(product)),
-              image: get_product_image(product)
-            }
-          } catch(err) {
-            Log.output(err + '\n' + JSON.stringify(shoppingResponse).substring(0,10000))
+
+  // Put product ID as key for easy lookup
+  var products = {}
+  var requestEntries = []
+  var batchMerchantIds = {}
+
+  Log.output('Calling Merchant Center for products: ' + productIds)
+
+  var product_id_prefix = 'online:' + contentLanguage + ':' + targetCountry + ':'
+
+  // Create one batch for each product
+  productIds.forEach(function (e, i) {
+
+    var composed_id = e.split(':')
+
+    var id = composed_id[0]
+    var merchantId = composed_id[1]
+    var offerId = composed_id[2]
+
+    requestEntries.push({
+      'batchId': id,
+      'merchantId': merchantId,
+      'method': 'get',
+      'productId': product_id_prefix.concat(offerId)
+    })
+
+    // Batches of 500
+    if ((i + 1) % 500 == 0 || i == productIds.length - 1) {
+
+      Logger.log('Request to shopping API with %s products: %s', requestEntries.length, requestEntries)
+      Log.output('Request to Merchant: ' + JSON.stringify(requestEntries))
+      var shoppingResponse = ShoppingContent.Products.custombatch({ 'entries': requestEntries })
+
+      Logger.log('Response from shopping API: ' + JSON.stringify(shoppingResponse).substring(0, 1000))
+      var entries = shoppingResponse.entries
+
+      entries.forEach(function (e) {
+
+        try {
+
+          var product = e.product
+
+          products[e.batchId] = {
+            title: get_product_title(product),
+            price: format_brazil_price(get_product_price(product)),
+            image: get_product_image(product)
           }
+        } catch (err) {
+          Log.output(err + '\n' + JSON.stringify(shoppingResponse).substring(0, 10000))
+        }
       })
-        
-        requestEntries = []
-        batchMerchantIds = {}
-      }
-    })
-    
-    return products
-  }
-  
-  function format_brazil_price(price) {
-    return 'R$ ' + price.replace(/,/g,'').replace(/\./g, ',')
-  }
-  
-  function get_product_price(product) {
-  
-    if (product.salePrice)
-      return product.salePrice.value
-    
-    if (product.price)
-      return product.price.value
-      
-    return find_custom_attribute(product, 'price') || find_custom_attribute(product, 'sale price') || ''
-  }
-  
-  function get_product_image(product) {
-    if (product.imageLink)
-      return product.imageLink
-      
-    return find_custom_attribute(product, 'image link') || ''
-  }
-  
-  function get_product_title(product) {
-    if (product.title)
-      return product.title
-      
-    return find_custom_attribute(product, 'title') || ''
-  }
-  
-  function find_custom_attribute(product, name) {
-    
-    var result = undefined
-  
-    if (!product.customAttributes)
-      return undefined
-    
-    product.customAttributes.some(function(field) {
-      if (field['name'] == name) {
-        result = field['value']
-        return true
-      }
-    })
-      
-    return result
-  }
+
+      requestEntries = []
+      batchMerchantIds = {}
+    }
+  })
+
+  return products
+}
+
+function format_brazil_price(price) {
+  return 'R$ ' + price.replace(/,/g, '').replace(/\./g, ',')
+}
+
+function get_product_price(product) {
+
+  if (product.salePrice)
+    return product.salePrice.value
+
+  if (product.price)
+    return product.price.value
+
+  return find_custom_attribute(product, 'price') || find_custom_attribute(product, 'sale price') || ''
+}
+
+function get_product_image(product) {
+  if (product.imageLink)
+    return product.imageLink
+
+  return find_custom_attribute(product, 'image link') || ''
+}
+
+function get_product_title(product) {
+  if (product.title)
+    return product.title
+
+  return find_custom_attribute(product, 'title') || ''
+}
+
+function find_custom_attribute(product, name) {
+
+  var result = undefined
+
+  if (!product.customAttributes)
+    return undefined
+
+  product.customAttributes.some(function (field) {
+    if (field['name'] == name) {
+      result = field['value']
+      return true
+    }
+  })
+
+  return result
+}
