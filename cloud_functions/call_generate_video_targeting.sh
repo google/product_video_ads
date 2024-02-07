0e8c96652e4d6d615888970a47562491e75aa53e
Kuba Rozek <kubarozek@google.com>
2024-02-07 10:42:07 +0000

    scripts to manually trigger CFs

---
+++ cloud_functions/call_generate_video_targeting.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+curl -m 70 -X POST https://europe-west1-rd-ytpva-prod61-dev-8aab.cloudfunctions.net/generate_video_targeting \
+-H "Authorization: bearer $(gcloud auth print-identity-token)"
