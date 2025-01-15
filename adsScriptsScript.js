0c294783adefb5c8de77e56197b3f848b804bc9d
Urs Enke <uenke@google.com>
2025-01-15 14:21:05 +0100

    Add illustration of functionality; document Ads Script part

--- adsScriptsScript.js
+++ adsScriptsScript.js
@@ -1,20 +1,26 @@
 const SPREADSHEET_URL =
   "https://docs.google.com/spreadsheets/d/1Dxfbnw2fV_1I9869AtrODBK99Zg4Z9kJg0SRZcdNP-8/edit";
 
+// Sheet names
 const AD_GROUP_SHEET_NAME = "AdGroups";
 const CONFIG_SHEET_NAME = "Base Config";
 const STATUS_SHEET_NAME = "Status";
 
+// Get data from sheets
 const SPREADSHEET = SpreadsheetApp.openByUrl(SPREADSHEET_URL);
 const AD_GROUP_SHEET = SPREADSHEET.getSheetByName(AD_GROUP_SHEET_NAME);
 const CONFIG_SHEET = SPREADSHEET.getSheetByName(CONFIG_SHEET_NAME);
 const STATUS_SHEET = SPREADSHEET.getSheetByName(STATUS_SHEET_NAME);
-
 const CAMPAIGN_NAME = CONFIG_SHEET.getRange(
   "googleAds_campaignName"
 ).getValue();
 const AD_GROUPS = getAdGroupsFromSheet();
 
+/**
+ * Gets a campaign (object) given its name.
+ * @param string name
+ * @returns
+ */
 function getCampaignByName(name) {
   const campaignIterator = AdsApp.videoCampaigns()
     .withCondition(`Name = "${name}"`)
@@ -25,6 +31,10 @@ function getCampaignByName(name) {
   return null;
 }
 
+/**
+ * Gets information on ad groups from the PVA spreadsheet.
+ * @returns object
+ */
 function getAdGroupsFromSheet() {
   const values = AD_GROUP_SHEET.getDataRange()
     .getValues()
@@ -48,10 +58,19 @@ function getAdGroupsFromSheet() {
   );
 }
 
+/**
+ * Determins whether the provided status equates to "enabled".
+ * @param string expectedStatus
+ * @returns boolean
+ */
 function shouldEnable(expectedStatus) {
   return expectedStatus.toLowerCase().trim() === "enabled";
 }
 
+/**
+ * Gets information on the video-related status of the desired ad groups.
+ * @returns object[]
+ */
 function getStatusFromSheet() {
   const values = STATUS_SHEET.getDataRange()
     .getValues()
@@ -66,12 +85,23 @@ function getStatusFromSheet() {
   }));
 }
 
+/**
+ * Writes the current data/time to the status sheet.
+ * @param string[] row
+ */
 function writeAdsUpdateTime(row) {
   STATUS_SHEET.getRange(row.rowNumber, 4, 1, 1).setValue(
     new Date().toISOString()
   );
 }
 
+/**
+ * Obtains ad-group object, if needed by creating the ad group.
+ * @param object campaign
+ * @param object adGroupInfo
+ * @param object item
+ * @returns object
+ */
 function getOrCreateAdGroup(campaign, adGroupInfo, item) {
   const adGroupIterator = campaign
     .videoAdGroups()
@@ -93,6 +123,10 @@ function getOrCreateAdGroup(campaign, adGroupInfo, item) {
   );
 }
 
+/**
+ * Pauses the ads in the given ad group.
+ * @param object adGroup
+ */
 function pauseExistingAds(adGroup) {
   const adGroupAdIterator = adGroup
     .videoAds()
@@ -103,6 +137,13 @@ function pauseExistingAds(adGroup) {
   }
 }
 
+/**
+ * Identifies the result of the given operation.
+ * @param object operation
+ * @param string entityName
+ * @param string entityId
+ * @returns object
+ */
 function getOperationResult(operation, entityName, entityId) {
   if (!operation.isSuccessful()) {
     throw new Error(
@@ -112,6 +153,13 @@ function getOperationResult(operation, entityName, entityId) {
   return operation.getResult();
 }
 
+/**
+ * Creates an ad in the given ad group.
+ * @param object adGroup
+ * @param object adGroupInfo
+ * @param object item
+ * @returns object
+ */
 function createAd(adGroup, adGroupInfo, item) {
   const videoCreationOperation = AdsApp.adAssets()
     .newYouTubeVideoAssetBuilder()
@@ -225,21 +273,37 @@ function createAd(adGroup, adGroupInfo, item) {
   }
 }
 
+/**
+ * Pauses all given ad groups.
+ * @param object adGroupIterator
+ */
 function pauseAdGroups(adGroupIterator) {
   while (adGroupIterator.hasNext()) {
     adGroupIterator.next().pause();
   }
 }
 
+/**
+ * Logs an error message and writes it to the status sheet.
+ * @param object error
+ * @param number rowNumber
+ */
 function handleError(error, rowNumber) {
   Logger.log(`[ERROR] ${error.message}`);
   STATUS_SHEET.getRange(rowNumber, 6, 1, 1).setValue(error.message);
 }
 
+/**
+ * Deletes the error output in the status sheet's given row number.
+ * @param number rowNumber
+ */
 function clearError(rowNumber) {
   STATUS_SHEET.getRange(rowNumber, 6, 1, 1).clearContent();
 }
 
+/**
+ * Updates ad groups based on the information provided in the PVA spreadsheet.
+ */
 function main() {
   const campaign = getCampaignByName(CAMPAIGN_NAME);
   if (!campaign) {
